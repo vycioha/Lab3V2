01fbf687fd7d75f5d7b6cb2388ff94ee1dcadb06
vpi <vytautaspikelis@hotmail.com>
2020-12-30 14:57:27 +0200

    minor fixes

--- src/FxControllers/MainWindowUser.java
+++ src/FxControllers/MainWindowUser.java
@@ -1,17 +1,31 @@
 package FxControllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.Course;
+import model.CourseIS;
 import model.User;
+import utils.DataRW;
+import utils.UserRW;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import static utils.DataRW.writeCourseISToFile;
 
 //implements Initializable -> Inteface, kuris pradiniam fxml formos langui priskirs reikšmes.
 public class MainWindowUser implements Initializable {
@@ -22,6 +36,27 @@ public class MainWindowUser implements Initializable {
     public Text profUsername;
     public Text profYear;
     public Text profBank;
+    public Button profUpdate;
+
+    public TextField profUserInput;
+    public TextField profSurnameInput;
+    public TextField profBirthYearInput;
+    public TextField profNameInput;
+    public TextField profBankAccInput;
+
+    private ObservableList<Course> courseObservableListIamEnrolled = FXCollections.observableArrayList();
+    private ObservableList<Course> courseObservableListAll = FXCollections.observableArrayList();
+
+    public ListView<String> CoursesIamEnrolledIn;
+    public Text IamEnrolledCourseTitle;
+    public Text IamEnrolledCourseStartDate;
+    public Text IamEnrolledCourseEndDate;
+
+    public ListView<String> AllCourses;
+    public Text AllCoursesCourseTitle;
+    public Text AllCoursesCourseStartDate;
+    public Text AllCoursesCourseEndDate;
+
     private User user;
     public Text loggedAs;
     public Text setProfileInformation = null;
@@ -34,10 +69,28 @@ public class MainWindowUser implements Initializable {
         loggedAs.setText("logged as: " + username);
     }
 
+    private CourseIS courseIS = new CourseIS();
+    private ArrayList<User> allUsers = new ArrayList<User>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        courseIS = DataRW.loadCourseISFromFile(courseIS);
+        allUsers = UserRW.GetAllUsers();
 
+        ArrayList<String> courseArrayListAll = new ArrayList<String>();
+
+        courseObservableListAll.addAll(courseIS.getAllCourses());
+
+        courseIS.getAllCourses().forEach(x -> courseArrayListAll.add(x.getCourseName()));
+
+        if(courseArrayListAll.size() > 0){
+            ObservableList<String> users = FXCollections.observableArrayList(courseArrayListAll);
+
+            AllCourses.getItems().addAll(users);
+        }
+        else{
+            AllCourses = new ListView<>();
+        }
     }
 
     public void LogOut() throws IOException {
@@ -51,4 +104,101 @@ public class MainWindowUser implements Initializable {
         stage.show();
     }
 
+    public void UpdateProfileInformation(ActionEvent actionEvent) throws IOException {
+        if(profUserInput != null &&
+            profSurnameInput != null &&
+            profNameInput != null &&
+            profBirthYearInput != null &&
+            profBankAccInput != null){
+
+            courseIS.getAllUsers().remove(user);
+            UserRW.RemoveUserDataFromFile(user);
+
+            user.setLogin(profUserInput.getText());
+            user.setName(profNameInput.getText());
+            user.setSurname(profSurnameInput.getText());
+                                            //teksto validacija - turi buti irasytas numeris - NEPADARYTA.
+            user.setYear(Integer.parseInt(profBirthYearInput.getText()));
+            user.setBankAccount(profBankAccInput.getText());
+
+            courseIS.getAllUsers().add(user);
+            DataRW.writeCourseISToFile(courseIS);
+            UserRW.WriteUserDataToFile(user);
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle(null);
+            alert.setHeaderText(null);
+            alert.setContentText("Not every field is filled");
+            alert.showAndWait();
+        }
+    }
+
+    public void LoadUserInformation(ActionEvent actionEvent) {
+        profUserInput.setText(user.getLogin());
+        profSurnameInput.setText(user.getSurname());
+        profBirthYearInput.setText(String.valueOf(user.getYear()));
+        profNameInput.setText(user.getName());
+        profBankAccInput.setText(user.getBankAccount());
+    }
+
+    public void GetCourseInfoFromEnrolled(MouseEvent mouseEvent) {
+        String courseName = CoursesIamEnrolledIn.getSelectionModel().getSelectedItem();
+
+        if(courseName != null){
+
+            Course course = courseIS.getCourseByName(courseName);
+
+            IamEnrolledCourseTitle.setText(course.getCourseName());
+            IamEnrolledCourseStartDate.setText(course.getCourseStart().toString());
+            IamEnrolledCourseEndDate.setText(course.getCourseEnd().toString());
+        }
+    }
+
+    public void GetCourseInfoFromAllCourses(MouseEvent mouseEvent) {
+        String courseName = AllCourses.getSelectionModel().getSelectedItem();
+
+        if(courseName != null){
+
+            Course course = courseIS.getCourseByName(courseName);
+
+            AllCoursesCourseTitle.setText(course.getCourseName());
+            AllCoursesCourseStartDate.setText(course.getCourseStart().toString());
+            AllCoursesCourseEndDate.setText(course.getCourseEnd().toString());
+        }
+    }
+
+    public void GetCoursesIamEnrolledin(ActionEvent actionEvent) {
+        ArrayList<String> courseArrayListIamEnrolled = new ArrayList<String>();
+
+        CoursesIamEnrolledIn.getItems().clear();
+
+        for(int i = 0; i < courseIS.getAllCourses().size(); i++){
+            Course course = courseIS.getCourseByName(AllCourses.getItems().get(i));
+
+            ArrayList<User> enrolledUsers = course.GetEnrolledUsers();
+
+            for(int j = 0; j < enrolledUsers.size(); j++){
+                if(enrolledUsers.get(j).getLogin().equals(user.getLogin())){
+                    courseObservableListIamEnrolled.add(course);
+                    courseArrayListIamEnrolled.add(course.getCourseName());
+                    break;
+                }
+            }
+        }
+
+        if(courseArrayListIamEnrolled.size() > 0){
+            ObservableList<String> courses = FXCollections.observableArrayList(courseArrayListIamEnrolled);
+
+            CoursesIamEnrolledIn.getItems().addAll(courses);
+        }
+        else{
+            CoursesIamEnrolledIn = new ListView<>();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle(null);
+            alert.setHeaderText(null);
+            alert.setContentText("You aren't enrolled in any course");
+            alert.showAndWait();
+        }
+    }
 }
--- src/utils/UserRW.java
+++ src/utils/UserRW.java
@@ -1,29 +1,69 @@
 package utils;
 
 import model.Course;
+import model.CourseIS;
 import model.User;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UserRW {
+
+//    public static User LoadUserFromFile(CourseIS courseIS) {
+//        try {
+//            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("2labDB.lib"));
+//            courseIS = (CourseIS) ois.readObject();
+//            ois.close();
+//        } catch (ClassNotFoundException e) {
+//            System.out.println("Failed with class recognition.");
+//        } catch (IOException e) {
+//            //e.printStackTrace();
+//            System.out.println("Failed to open file.");
+//        }
+//        return courseIS;
+//    }
+//
+//    public static void writeCourseISToFile(CourseIS courseIS) {
+//        try {
+//            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("2labDB.lib"));
+//            out.writeObject(courseIS);
+//            out.close();
+//        } catch (IOException e) {
+//            System.out.println("Fail.\n");
+//        }
+//    }
 
     public static void WriteUserDataToFile(User user) {
         try {
             String nameOfFile = "Users.lib";
-            new File("C:/Users/vpi/IdeaProjects/Lab3V2/" + nameOfFile);
-
+            new File("../Lab3V3/" + nameOfFile);
             BufferedWriter outStream = new BufferedWriter(new FileWriter(nameOfFile, true));
 
-            outStream.write("\n" + user.getName() + ";" + user.getSurname() + ";" + user.getBankAccount() + ";" + user.getYear() + ";" + user.isModerator() + ";" + user.getLogin() + ";" + user.getPsw() + ";" + user.getMyEnrolledCourses());
+            outStream.write(user.getName() + ";" + user.getSurname() + ";" + user.getBankAccount() + ";" + user.getYear() + ";" + user.isModerator() + ";" + user.getLogin() + ";" + user.getPsw() + ";" + user.getMyEnrolledCourses() + "\n");
             outStream.close();
         } catch (IOException e) {
             System.out.println("Fail.\n");
         }
     }
 
+    public static void RemoveUserDataFromFile(User user) throws IOException {
+        ArrayList<User> users = GetAllUsers();
+
+        int index = users.indexOf(users.stream().filter(x -> x.getLogin().equals(user.getLogin())).findFirst().orElse(null));
+
+        users.remove(index);
+
+        Files.newBufferedWriter(Paths.get("../Lab3V3/Users.lib"), StandardOpenOption.TRUNCATE_EXISTING);
+
+        users.forEach(x -> WriteUserDataToFile(x));
+    }
+
     public static ArrayList<User> GetAllUsers(){
-        File file = new File("C:/Users/vpi/IdeaProjects/Lab3V2/Users.lib");
+        File file = new File("../Lab3V3/Users.lib");
         BufferedReader br = null;
         ArrayList<User> users = new ArrayList<User>();
         try {
@@ -42,7 +82,7 @@ public class UserRW {
     }
 
     public static User ReadUser(String username){
-        File file = new File("C:/Users/vpi/IdeaProjects/Lab3V2/Users.lib");
+        File file = new File("../Lab3V3/Users.lib");
         BufferedReader br = null;
         ArrayList<User> users = new ArrayList<User>();
         try {
